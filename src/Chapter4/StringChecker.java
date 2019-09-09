bf2af10af239d4be0c71722d0d98724264cd5d3a
kimba <kimba@10.0.0.184>
2019-09-08 20:43:33 -0400

    incomplete

--- src/Chapter4/StringChecker.java
+++ src/Chapter4/StringChecker.java
@@ -16,6 +16,7 @@ public class StringChecker
 
 	public StringChecker(String s)
 	{
+		this.setString(s);
 	}
 
    public void setString(String s)
@@ -25,11 +26,32 @@ public class StringChecker
 
 	public boolean findLetter(char c)
 	{
+		for(int x = 0; x < word.length()-1; x ++) {
+			
+			if(word.charAt(x) == c) {
+				return true;
+			}
+			
+		}
 		return false;
 	}
 
 	public boolean findSubString(String s)
 	{
+
+		for(int x = 0; x < word.length()-1; x ++) {
+			
+			out.println(word.substring(x, s.length()-1) );
+//			if(word.charAt(x) == s.charAt(0)) {
+//				out.println(word.substring(x, s.length() -1));
+//			
+////				if(word.substring(x, s.length()-1) == s) {
+////					return true;
+////				}
+//				
+//			}
+		
+		}
 		return false;
 	}
 
