3d89b2031a43c85b3768aa6f376a8e794ea3bb08
Cimball1334 <46455722+Cimball1334@users.noreply.github.com>
2019-09-19 08:35:23 -0400

    Add files via upload

---
+++ src/Chapter7/HexToBinary.java
@@ -0,0 +1,41 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*; 
+
+public class HexToBinary
+{
+	private char hex;
+
+	public HexToBinary()
+	{
+
+
+	}
+
+	public HexToBinary(char hexNum)
+	{
+
+
+	}
+
+	public void setHex(char hexNum)
+	{
+
+
+	}
+
+	public String getBinary()
+	{
+
+		return "";
+	}
+
+	public String toString()
+	{
+		return "";
+	}
+}
---
+++ src/Chapter7/HexToBinaryRunner.java
@@ -0,0 +1,17 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*; 
+import java.util.Scanner;
+
+public class HexToBinaryRunner
+{
+	public static void main( String args[] )
+	{
+		//add test cases	
+
+	}
+}
