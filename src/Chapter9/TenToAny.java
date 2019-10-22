89f403c0bb785fa79daaa64fbc91209119e3ec2d
kimballcraigj <kimballcraigj@BP0105044211NZW.bp.scps.k12.va.us>
2019-10-22 09:08:16 -0400

    im going to cry

--- src/Chapter9/TenToAny.java
+++ src/Chapter9/TenToAny.java
@@ -10,16 +10,35 @@ public class TenToAny
    private int base10;
    private int newBase;
 
-	//add constructors
-
-	//add a set method
-
+   public TenToAny() {
+   
+   }
+   public TenToAny(int num, int base) {
+	setNums(num,base);
+   }
+   public void setNums(int num, int base) {
+	   base10 = num;
+	   newBase = base;
+   }
+   
 	public String getNewNum()
 	{
-		String newNum="";
+		String newNum="", reverse = "";
+		
+		
+		while( base10 >0 ){
+		 
+		
+			
+		}
+		
+		
+		
 		return newNum;
 	}
 
 	//add a toString method	
-	
+	public String toString() {
+		return getNewNum();
+	}
 }
--- src/Chapter9/TenToAnyRunner.java
+++ src/Chapter9/TenToAnyRunner.java
@@ -11,6 +11,10 @@ public class TenToAnyRunner
 	public static void main( String args[] )
 	{
 		//add test cases	
+		TenToAny test = new TenToAny();
+		
+		test.setNums(10, 2);
+		System.out.println(test);
 		
 	}
 }
