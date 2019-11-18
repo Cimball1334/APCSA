98d87ce5a57249087b9a154aa0a5ec39a67e01d7
kimba <kimba@10.0.0.184>
2019-11-18 18:25:24 -0500

    i hate nested loops

--- src/Chapter11/TriangleWord.java
+++ src/Chapter11/TriangleWord.java
@@ -7,7 +7,7 @@ package Chapter11;
 
 class TriangleWord
 {
-   private String word;
+   private String word = " ";
 
 	public TriangleWord()
 	{
@@ -15,15 +15,30 @@ class TriangleWord
 
 	public TriangleWord(String w)
 	{
+		setWord(w);
 	}
 
 	public void setWord(String w)
 	{
+		word = w;
 	}
 
 	public String toString()
 	{
-		String output="";
-		return output+"\n";
+		   String output = " ";
+		   
+		   int len = word.length()-1, idx = 1;
+
+		    System.out.println(String.format("%"+(len+1)+"s", word.charAt(0)));
+		    for(int x=0; x<word.length()-2; x++){
+		        System.out.print(String.format("%"+(len--)+"s", word.charAt(idx)));
+		        System.out.println(String.format("%"+(idx*2)+"s", word.charAt(idx++)));
+		    }
+		    System.out.println(new StringBuilder(word.substring(1)).reverse().toString() + word);
+		
+		   		   
+		return output;
 	}
+	
+	
 }
--- src/Chapter12/LineCounter.java
+++ src/Chapter12/LineCounter.java
@@ -1,3 +1,5 @@
+package Chapter12;
+
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
