d067d665978724b578b9fbc72587644dc51a04cf
Cimball1334 <46455722+Cimball1334@users.noreply.github.com>
2019-11-19 08:02:59 -0500

    Add files via upload

--- src/Chapter12/LineBreaker.java
+++ src/Chapter12/LineBreaker.java
@@ -1,5 +1,3 @@
-package Chapter12;
-
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -10,43 +8,54 @@ import java.util.Scanner;
 
 public class LineBreaker
 {
-   private String line;
-   private int breaker;
+    private String line;
+    private int breaker;
 
-   public LineBreaker()
-   {
-   	this("",0);
-   }
+    public LineBreaker()
+    {
+        setLineBreaker("",0);
+    }
 
-   public LineBreaker(String s, int b)
-   {
-   }
+    public LineBreaker(String s, int b)
+    {
+        setLineBreaker(s,b);
+    }
 
-	public void setLineBreaker(String s, int b)
-	{
-	}
+    public void setLineBreaker(String s, int b)
+    {
+        line = s;
+        breaker = b;
+    }
 
-	public String getLine()
-	{
-		return "";
-	}
+    public String getLine()
+    {
+        return line;
+    }
 
-	public String getLineBreaker()
-	{
-		String box ="";
+    public String getLineBreaker()
+    {
+        String box ="";
+        int counter = 0;
+        Scanner chopper = new Scanner(line);
+        
+        while(chopper.hasNext())
+        {
+            box += chopper.next();
+            counter++;
+           if(counter == breaker)
+           {
+             counter = 0;
+             box += "\n";
+            }
+        }
 
 
+        return box;
+    }
 
-
-
-
-
-
-		return box;
-	}
-
-	public String toString()
-	{
-		return "";
-	}
+    public String toString()
+    {
+        String output = getLine()+ "\n";
+        return output += getLineBreaker() + "\n";
+    }
 }
--- src/Chapter12/LineBreakerRunner.java
+++ src/Chapter12/LineBreakerRunner.java
@@ -1,5 +1,3 @@
-package Chapter12;
-
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -12,5 +10,20 @@ public class LineBreakerRunner
 {
    public static void main(String args[])
    {
+       LineBreaker test = new LineBreaker();
+       
+       test.setLineBreaker("1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9",4);
+       System.out.println(test);
+       
+       test.setLineBreaker("t h e b i g b a d w o l f h a d b i g e a r s a n d t e e t h",2);
+       System.out.println(test);
+       
+       test.setLineBreaker("a c o m p u t e r s c i e n c e p r o g r a m",7);
+       System.out.println(test);
+       
+       test.setLineBreaker("i a m s a m i a m",2);
+       System.out.println(test);
+       
+       
 	}
 }
