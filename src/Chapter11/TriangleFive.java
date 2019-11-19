a08e42485dc385e9911da19e11df5fed542afe7e
Cimball1334 <46455722+Cimball1334@users.noreply.github.com>
2019-11-19 08:17:01 -0500

    Add files via upload

--- src/Chapter11/TriangleFive.java
+++ src/Chapter11/TriangleFive.java
@@ -1,4 +1,3 @@
-package Chapter11;
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -7,28 +6,53 @@ package Chapter11;
 
 public class TriangleFive
 {
-   private char letter;
-   private int amount;
+    private char letter;
+    private int amount;
 
-	public TriangleFive()
-	{
-	}
+    public TriangleFive()
+    {
+    }
 
-	public TriangleFive(char c, int amt)
-	{
-	}
+    public TriangleFive(char c, int amt)
+    {
+        setLetter(c);
+        setAmount(amt);
+    }
 
-	public void setLetter(char c)
-	{
-	}
+    public void setLetter(char c)
+    {
+        letter = c;
+    }
 
-	public void setAmount(int amt)
-	{
-	}
+    public void setAmount(int amt)
+    {
+        amount = amt;
+    }
 
-	public String toString()
-	{
-		String output="";
-		return output;
-	}
+    public String toString()
+    {
+        String output="";
+        int fakeAmount = amount;
+        char fakeLetter = letter;
+        for(int k = amount; k >= 1; k--)
+        {
+            fakeAmount = amount;
+            letter = fakeLetter;
+        for(int i = 1; i  <= k; i++)
+        {
+            for(int j = fakeAmount; j >= 1;  j--)
+            {
+                if(letter > 90) letter =(char)(letter - 26);
+                output += letter;
+               
+            }
+            output += " ";
+            fakeAmount--;
+            letter++;
+        }
+        output += "\n";
+    }
+        return output + "\n";
+    
+}
 }
--- src/Chapter11/TriangleFiveRunner.java
+++ src/Chapter11/TriangleFiveRunner.java
@@ -1,4 +1,3 @@
-package Chapter11;
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -9,5 +8,28 @@ public class TriangleFiveRunner
 {
    public static void main(String args[])
    {
+       TriangleFive test = new TriangleFive();
+       
+       test.setLetter('C');
+       test.setAmount(4);
+       System.out.println(test);
+       
+       test.setLetter('A');
+       test.setAmount(5);
+       System.out.println(test);
+       
+       test.setLetter('B');
+       test.setAmount(7);
+       System.out.println(test);
+       
+       test.setLetter('X');
+       test.setAmount(6);
+       System.out.println(test);
+       
+       test.setLetter('Z');
+       test.setAmount(8);
+       System.out.println(test);
+       
+       
 	}
 }
