a69638eb8d2033be5943d656d9ac2b2a46527fff
kimballcraigj <kimballcraigj@BP0105044211NZW.bp.scps.k12.va.us>
2019-12-04 09:08:13 -0500

    wqol

--- src/Chapter14/ArrayFunHouseTwo.java
+++ src/Chapter14/ArrayFunHouseTwo.java
@@ -9,31 +9,72 @@ package Chapter14;
 import java.lang.System;
 import java.lang.Math;
 
-public class ArrayFunHouseTwo
-{
-	//goingUp() will return true if all numbers
-	//in numArray are in increasing order
-	//[1,2,6,9,23] returns true
-	//[9, 11, 13, 8]  returns false
-	public static boolean goingUp(int[] numArray)
-	{
-		return true;
+public class ArrayFunHouseTwo {
+	// goingUp() will return true if all numbers
+	// in numArray are in increasing order
+	// [1,2,6,9,23] returns true
+	// [9, 11, 13, 8] returns false
+	public static boolean goingUp(int[] numArray) {
+		boolean flag = false;
+
+		for (int i = 0; i < numArray.length - 1; i++) {
+
+			if (numArray[i + 1] > numArray[i]) {
+				flag = true;
+			} else {
+				flag = false;
+			}
+
+		}
+
+		return flag;
 	}
 
-	//goingDown() will return true if all numbers
-	//in numArray are in decreasing order
-	//[31,12,6,2,1] returns true
-	//[31, 20, 10, 15, 9] returns false
-	public static boolean goingDown(int[] numArray)
-	{
-		return true;
+	// goingDown() will return true if all numbers
+	// in numArray are in decreasing order
+	// [31,12,6,2,1] returns true
+	// [31, 20, 10, 15, 9] returns false
+	public static boolean goingDown(int[] numArray) {
+		boolean flag = false;
+
+		for (int i = 0; i < numArray.length - 1; i++) {
+			if (numArray[i + 1] < numArray[i]) {
+				flag = true;
+			} else {
+				flag = false;
+				break;
+			}
+		}
+
+		return flag;
 	}
 
-	//getValuesBiggerThanX will return an array that contains
-	//count number of values that are larter than parameter x
-	//[1,2,3,4,5,6,7,8,9,10,11,6],3,5  would return [6,7,8]
-	public static int[] getCountValuesBiggerThanX(int[] numArray, int count, int x)
-	{
-		return null;
+	// getValuesBiggerThanX will return an array that contains
+	// count number of values that are larter than parameter x
+	// [1,2,3,4,5,6,7,8,9,10,11,6],3,5 would return [6,7,8]
+	public static int[] getCountValuesBiggerThanX(int[] numArray, int count, int x) {
+		
+		int counting = 0;
+		
+		for (int i = 0; i < count; i++) {
+
+			if (numArray[i] > x)
+				counting++;
+
+		}
+		int[] tom = new int[counting];
+		counting = 0;
+
+		for (int i = 0; i < count; i++) {
+
+			if (numArray[i] > x) {
+
+				tom[counting] = numArray[i];
+				counting++;
+
+			}
+		}
+
+		return tom;
 	}
 }
--- src/Chapter14/ArrayFunHouseTwoRunner.java
+++ src/Chapter14/ArrayFunHouseTwoRunner.java
@@ -8,10 +8,36 @@ package Chapter14;
 
 import java.util.Arrays;
 
-public class ArrayFunHouseTwoRunner
-{
+public class ArrayFunHouseTwoRunner {
 	public static void main( String args[] )
 	{
-		//add test cases
+	
+		int[] one   = {1,2,3,4,5,6,7,8,9,10};
+		int[] two   = {1,2,3,9,11,20,30};
+		int[] three = {9,8,7,6,5,4,3,2,0,-2};
+		int[] four  = {3,6,9,12,15,18,21,23,19,17,15,13,11,10,6,3,2,1};
+	
+		System.out.println(Arrays.toString(one));
+		System.out.println("is going up? " + ArrayFunHouseTwo.goingUp(one));
+		System.out.println(Arrays.toString(two));
+		System.out.println("is going up? " + ArrayFunHouseTwo.goingUp(two));
+		System.out.println(Arrays.toString(three));
+		System.out.println("is going up? " + ArrayFunHouseTwo.goingUp(three));
+		System.out.println(Arrays.toString(four));
+		System.out.println("is going up? " + ArrayFunHouseTwo.goingUp(four) + "\n\n");
+		
+		System.out.println(Arrays.toString(one));
+		System.out.println("is going down? " + ArrayFunHouseTwo.goingDown(one));
+		System.out.println(Arrays.toString(two));
+		System.out.println("is going down? " + ArrayFunHouseTwo.goingDown(two));
+		System.out.println(Arrays.toString(three));
+		System.out.println("is going down? " + ArrayFunHouseTwo.goingDown(three));
+		System.out.println(Arrays.toString(four));
+		System.out.println("is going down? " + ArrayFunHouseTwo.goingDown(four)+ "\n\n");
+		
+		System.out.println(Arrays.toString(four));
+		System.out.println("first 7 values greater than 9 " + Arrays.toString(ArrayFunHouseTwo.getCountValuesBiggerThanX(four, 7, 9)));
+		
+		
 	}
 }
