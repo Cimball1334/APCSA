38ed45325a267eeee34631ccedbe76c3eb6a36d8
Cimball1334 <46455722+Cimball1334@users.noreply.github.com>
2019-12-16 08:47:23 -0500

    Add files via upload

--- src/Chapter14/Fibonacci.java
+++ src/Chapter14/Fibonacci.java
@@ -1,20 +1,45 @@
-package Chapter14;
-
-///© A+ Computer Science  -  www.apluscompsci.com
+//© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
 //Lab  -
 
-public class Fibonacci
-{
-	//instance variable
+public class Fibonacci {
+    private int length;
+    private int[] sequence;
 
-	//constructors
 
-	//set method
+    public Fibonacci() {
+        setVars(99);
+    }
 
-	//get method
+    public Fibonacci(int size) {
+        setVars(size);
+    }
 
-	//toString
+    public void setVars(int size) {
+        length = size;
+        int num = 0;
+        int num2 = 1;
+        int num3 = 0;
+        sequence = new int[length + 1];
+        for (int i = 0; i < length; i++) {
+            num3 = num + num2;
+            num = num2;
+            num2 = num3;
+            sequence[i] = num;
+        }
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public int getValueAtIndex(int index) {
+        index++;
+        if(index > length || index < 0){
+            return -1;
+        }
+        return sequence[index];
+    }
 }
