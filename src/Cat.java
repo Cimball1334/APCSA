ba2701b6f2b5de2616c5c5a60a065db2d43e20df
kimballcraigj <kimballcraigj@BP0105044211NZW>
2020-01-13 09:06:28 -0500

    dgsfgjckvbo

--- src/Cat.java
+++ src/Cat.java
@@ -2,6 +2,7 @@
 public class Cat {
 
 	private String name;
+	public int lives;
 	
 	public Cat() {
 		
@@ -11,7 +12,12 @@ public class Cat {
 		setName(name);
 		
 	}
-	
+	public Cat(String name, int lives) {
+		//decleration of name data type : String
+		setName(name);
+		lives = lives;
+		
+	}
 	//getter and setter methods for initilization of static class and unstatic deferables.
 	
 	//*************
--- src/corn.java
+++ src/corn.java
@@ -2,7 +2,7 @@
 public class corn {
 
 	public static void main(String[]args) {
-		Cat jamie = new Cat("jamie");
+		Cat jamie = new Cat("jamie", 10);
 		System.out.println(" my cats name is : " + jamie.getName());
 	}
 	
